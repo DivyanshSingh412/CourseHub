a858678a4f1cc79bbcb85e09ff7c5d5ea17ecdbb
DivyanshSingh412 <singhdivy012@gmail.com>
2025-12-21 20:18:06 +0530

    Initial clean commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 
@@ -10,7 +9,6 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [
-      tailwindcss(),
       react()
     ],
     server: {
