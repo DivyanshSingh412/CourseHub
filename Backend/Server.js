ad0011e24c9cbd355e5b96232e5b0bc8143621f6
DivyanshSingh412 <singhdivy012@gmail.com>
2026-04-22 01:47:49 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -11,7 +11,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     https://course-hub-jw1i.vercel.app/,
-    ]
+    ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
