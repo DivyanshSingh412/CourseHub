b6e03672b84883a4777850f7f2db0466c0c6b29f
DivyanshSingh412 <singhdivy012@gmail.com>
2026-04-22 01:52:48 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    https://course-hub-jw1i.vercel.app/,
+    'https://course-hub-jw1i.vercel.app/'
     ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
